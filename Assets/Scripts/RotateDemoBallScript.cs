ed42b501b9ac7ac8b0dbe8c47aa1b3958f1d9076
Arthur K Neto <arthurklugneto@gmail.com>
2019-07-09 21:53:07 -0300

    Inicio do Projeto

---
+++ Assets/Scripts/RotateDemoBallScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateDemoBallScript : MonoBehaviour
+{
+
+    public GameObject poolBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolBall = GameObject.Find("PoolBall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        poolBall.transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+
+}
