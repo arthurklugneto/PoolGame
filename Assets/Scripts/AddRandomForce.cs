d03d00e35c126cee9189bec6135620ed47054ba6
Arthur K Neto <arthurklugneto@gmail.com>
2019-07-11 22:57:13 -0300

    Gravidade e Medidas corrigidas

---
+++ Assets/Scripts/AddRandomForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRandomForce : MonoBehaviour
+{
+    private Rigidbody rb;
+    public int minimum = -10;
+    public int maximum = 10;
+    private static readonly System.Random random = new System.Random();
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Random force added to object");
+            rb = GetComponent<Rigidbody>();
+            rb.AddForce(random.Next(minimum,maximum), 0, random.Next(minimum, maximum));
+        }
+    }
+}
